725fc9e213817805ca08543cd7123fd15b12806f
Andrew894 <bella14@spu.edu>
2026-04-24 14:18:56 -0700

    Game

--- game.js
+++ game.js
@@ -8,27 +8,53 @@ const htmlColors = [
     "Violet", "White", "Yellow"
 ];
 
-const attempt = document.getElementById("attemptBtn");
-const correctColor = document.getElementById("correctColor");
-//const messageDiv = document.getElementById("message");
-//const score = document.getElementById("score");
+const attemptBtn = document.getElementById("attemptBtn");
+const correctColorDiv = document.getElementById("correctColor");
+const messageDiv = document.getElementById("message");
+const scoreDiv = document.getElementById("score");
+
+let currentColor = "";
 
 function showMessage(message){
     messageDiv.textContent = message;
 }
 
+function showScore(score){
+    scoreDiv.textContent = score;
+}
+
 
 function getRandomColor(){
-    correctColor.style.backgroundColor = htmlColors[Math.floor(Math.random * htmlColors.lentgh)];
+    correctColor.style.backgroundColor = htmlColors[Math.floor(Math.random() * htmlColors.length)];
+    correctColorDiv.style.backgroundColor = correctColor;
 }
 
-function attempt(){
-    let lives = 3;
+attemptBtn.addEventListener("click", function(){
+    const guess = prompt("Guess the color: ")
+    let attempt = 3;
+    let score = 0;
 
-    if(attempt === correctColor){
+    if ((guess && guess.toLowerCase() === currentColor.toLowerCase()) && attempt === 3) {
         showMessage("Correct");
-
+        score += 100;
+        showScore(score);
+    } else if ((guess && guess.toLowerCase() === currentColor.toLowerCase()) && attempt === 2) {
+        showMessage("Correct");
+        score += 50;
+        showScore(score);
+    } else if ((guess && guess.toLowerCase() === currentColor.toLowerCase()) && attempt === 1) {
+        showMessage("Correct");
+        score += 10;
+        showScore(score);
+    } else {
+        showMessage("Wrong! It was " + currentColor);
+        attempt -= 1;
     }
-}
+
+    if (attempt === 0){
+        showMessage("Game Over");
+        return showScore(score);
+    }
+})
 
 getRandomColor();
