de2161b7892e76d46b2c4bbbee7b2a7ce2eb2468
Andrew894 <bella14@spu.edu>
2026-04-24 13:38:03 -0700

    Colors

---
+++ game.js
@@ -0,0 +1,32 @@
+const htmlColors = [
+    "Black", "Blue", "BlueViolet",
+    "Brown", "Gold", "Gray",
+    "Green", "Lime", "Magenta",
+    "Orange", "Pink", "Purple",
+    "Red", "SeaGreen", "SeaShell",
+    "SkyBlue", "Teal", "Turquoise",
+    "Violet", "White", "Yellow"
+];
+
+const attempt = document.getElementById("attemptBtn");
+const correctColor = document.getElementById("correctColor");
+const messageDiv = document.getElementById("message");
+const score = document.getElementById("score");
+
+function showMessage(message){
+    messageDiv.textContent = message;
+}
+
+
+function getRandomColor(){
+    correctColor.style.backgroundColor = htmlColors[Math.floor(Math.random * htmlColors.lentgh)];
+}
+
+function attempt(){
+    let lives = 3;
+
+    if(attempt === correctColor){
+        showMessage("Correct");
+
+    }
+}
